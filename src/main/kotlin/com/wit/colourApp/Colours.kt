1dd8d3df267e6c18cd56a2c1869427639df2601d
Namra <20094326@mail.wit.ie>
2022-02-19 14:30:40 +0000

    Implementing the firstColour function

--- src/main/kotlin/com/wit/colourApp/Colours.kt
+++ src/main/kotlin/com/wit/colourApp/Colours.kt
@@ -15,8 +15,8 @@ class Colours {
     }
 
     fun firstColour() : String{
-        //TODO return the first colour in the list
-        return "this should return the first colour in the list"
+        // return the first colour in the list
+        return colours.first()
     }
 
     fun lastColour() : String{
