e73dd6cc4ff12501835771c752b34d943cbcf899
Namra <20094326@mail.wit.ie>
2022-02-19 16:16:00 +0000

    printing list of colours in the required format

--- src/main/kotlin/com/wit/colourApp/Main.kt
+++ src/main/kotlin/com/wit/colourApp/Main.kt
@@ -40,11 +40,14 @@ fun main(args: Array<String>){
     println("True or false, is blue in the list: ${colours.isColourInTheList("blue")} ")
 
     println("The list of colours, each printed on a new line is:")
-    //TODO Write Code here (below this comment) to print the list in this format:
+    //  Write Code here (below this comment) to print the list in this format:
     //    Colour: Red
     //    Colour: Orange
     //    ...
     //    Colour: Silver
+    for(colour in colours.getAll()){
+        println("Colour: $colour")
+    }
 
     // for the three additional functions you wrote in Colours.kt, print the returned value here.
 
